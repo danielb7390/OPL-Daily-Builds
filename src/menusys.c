4231e97de63028a89995c09b7a797297d50d5adf
Bruno Daniel <danielb7390@sapo.pt>
2017-10-01 15:40:21 +0100

    Add different Main/Info page for ELM page!

--- src/menusys.c
+++ src/menusys.c
@@ -638,11 +638,12 @@ void menuHandleInputMenu()
 void menuRenderMain()
 {
     // selected_item can't be NULL here as we only allow to switch to "Main" rendering when there is at least one device activated
+	theme_element_t* elem = NULL;
 	item_list_t *list = selected_item->item->userdata;
-	if(list && list->mode == ELM_MODE){
-		theme_element_t* elem = gTheme->mainElemsELM.first;
+	if(list && gTheme->mainElemsELM.first && list->mode == ELM_MODE){
+		elem = gTheme->mainElemsELM.first;
     }else{
-        theme_element_t* elem = gTheme->mainElems.first;
+        elem = gTheme->mainElems.first;
     }
     
     while (elem) {
@@ -716,12 +717,12 @@ void menuRenderInfo()
     _menuRequestConfig();
 
     WaitSema(menuSemaId);
-
+	theme_element_t* elem = NULL;
 	item_list_t *list = selected_item->item->userdata;
-	if(list && list->mode == ELM_MODE){
-		theme_element_t* elem = gTheme->infoElemsELM.first;
+	if(list && gTheme->infoElemsELM.first && list->mode == ELM_MODE){
+		elem = gTheme->infoElemsELM.first;
     }else{
-        theme_element_t* elem = gTheme->infoElems.first;
+        elem = gTheme->infoElems.first;
     }
     
     while (elem) {
